7f7106919f0110fd6f1a47a0f3d9156b082c1148
Javaadm <mr.serega20@mail.ru>
2021-04-25 04:48:00 +0700

    initial2

---
+++ Dron/Dron.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Dron;
+
+namespace Dron
+{
+    public class Dron
+    {
+        private float ChanceEvasion = 0.15; 
+
+
+        private DronWeapon Weapon { get; set; }
+
+        public Dron(DronWeapon _weapon)
+        {
+            Weapon = _weapon;
+        }
+
+        public void ChangeWeapon(DronWeapon _weapon)
+        {
+            Weapon = _weapon;
+        }
+
+        public void DoAttack()
+        {
+            Weapon.Shot();
+        }
+
+        private bool EvasionAvailable()
+        {
+            return random.NextDouble() <= ChanceEvasion;
+        }
+        public void MoveInOneStep()
+        {
+            if (EvasionAvailable())
+            {
+                DoEvasion
+            }
+        }
+    }
+}
